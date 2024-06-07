c34d94f1fbbd95a1103fb6e5ad7d924bb873f63b
Rafael Masoni <oi@rmasoni.com>
2024-06-07 05:48:39 -0300

    Add initial content

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 1500,
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 200,
+  speed: 2000,
+  centeredSlides: true,
+  loop: true,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
+});
